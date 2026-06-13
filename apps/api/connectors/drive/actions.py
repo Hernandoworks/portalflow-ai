9e1da9adc65baa4146591e549b4b1552f2be37ed
Hernandoworks <hernando.ren@studytafensw.edu.au>
2026-06-13 15:43:18 +1000

    feat: add Google Drive connector adapter skeleton

---
+++ apps/api/connectors/drive/actions.py
@@ -0,0 +1,9 @@
+def execute(action, payload):
+    # Google Drive OAuth adapter placeholder
+    # TODO: connect google-api-python-client
+    return {
+        'connector':'drive',
+        'action':action,
+        'status':'received',
+        'payload':payload
+    }
